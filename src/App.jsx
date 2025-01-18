e0ca9604ae4854c5699f452d917e4584200639aa
N3a3i3m3a <nishimwenaima3@gmail.com>
2025-01-18 18:27:33 +0200

    routes correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import Home from "./components/Home";
 import About from "./components/About"
 import Skills from "./components/Skills"
 import Projects from "./components/Projects";
